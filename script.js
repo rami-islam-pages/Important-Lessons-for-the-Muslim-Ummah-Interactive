a064ef1914ee6e08e1c740b7f38d1dc49a293ce7
Rami <ramirazaq@gmail.com>
2025-11-15 08:10:47 -0600

    Create homepage with Islamic book cover design

---
+++ script.js
@@ -0,0 +1,6 @@
+function enterBook() {
+    // Placeholder for navigation to the next page
+    // You can update this to navigate to your content pages
+    alert('Welcome! Content pages coming soon...');
+    // Example: window.location.href = 'chapters.html';
+}
