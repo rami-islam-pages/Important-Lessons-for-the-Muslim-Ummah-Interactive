5f3b170d1199b4e4e96e558af44f44495a58ca85
Rami <ramirazaq@gmail.com>
2025-11-17 08:38:20 -0600

    Organize codebase: Add .gitignore, update README with professional documentation, rename lessons (Intro & Lesson 1), add code documentation

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+/**
+ * Homepage Script
+ * Handles navigation from landing page to lessons
+ * 
+ * Important Lessons for the Muslim Ummah - Interactive Website
+ * By Sheikh Abdul Aziz bin Abdullah Bin Baz (rahimahullah)
+ * Explained by Sheikh Haytham ibn Muhammad Jameel Sarhaan (hafidhahullah)
+ */
+
+/**
+ * Navigate to the lessons overview page
+ */
 function enterBook() {
-    // Navigate to lessons page
     window.location.href = 'lessons.html';
 }
